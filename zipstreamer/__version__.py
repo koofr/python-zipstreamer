a7d083217fd06aea588e7bea65c132523ac4f198
Luka Zakrajsek <luka@bancek.net>
2018-03-15 11:56:51 +0100

    Fix linting errors

--- zipstreamer/__version__.py
+++ zipstreamer/__version__.py
@@ -1,5 +1,10 @@
+# pylint: disable=missing-docstring
+
 __title__ = 'zipstreamer'
-__description__ = 'Python library for generating ZIP files on-the-fly with ZIP file size information.'
+__description__ = (
+    'Python library for generating ZIP files on-the-fly with ZIP file'
+    ' size information.'
+)
 __url__ = 'http://github.com/koofr/python-zipstreamer'
 __version__ = '0.1.0'
 __build__ = 0x000100
